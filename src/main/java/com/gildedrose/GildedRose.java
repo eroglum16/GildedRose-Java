575e6a4b8c51a90f65d66c62f55f9382505cdfd2
eroglum16 <m.eroglu5122@gmail.com>
2024-11-10 12:20:31 +0100

    Add strategy for updating legendary items

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,7 @@ package com.gildedrose;
 
 import com.gildedrose.strategy.AgedProductDailyUpdateStrategy;
 import com.gildedrose.strategy.DailyUpdateStrategy;
+import com.gildedrose.strategy.LegendaryItemUpdateStrategy;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,11 +11,14 @@ class GildedRose {
     Item[] items;
 
     private final List<String> agedProducts = Arrays.asList("Aged Brie");
+    private final List<String> legendaryItems = Arrays.asList("Sulfuras, Hand of Ragnaros");
 
     private final DailyUpdateStrategy agedProductDailyUpdateStrategy;
+    private final DailyUpdateStrategy legendaryItemUpdateStrategy;
     public GildedRose(Item[] items) {
         this.items = items;
         this.agedProductDailyUpdateStrategy = new AgedProductDailyUpdateStrategy();
+        this.legendaryItemUpdateStrategy = new LegendaryItemUpdateStrategy();
     }
 
     public void updateQuality() {
@@ -22,12 +26,13 @@ class GildedRose {
             if (this.agedProducts.contains(item.name)){
                 this.agedProductDailyUpdateStrategy.update(item);
                 return;
+            } else if (this.legendaryItems.contains(item.name)){
+                this.legendaryItemUpdateStrategy.update(item);
+                return;
             }
             if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
+                    item.quality = item.quality - 1;
                 }
             } else {
                 if (item.quality < 50) {
@@ -47,16 +52,12 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
+            item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
                 if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
+                        item.quality = item.quality - 1;
                     }
                 } else {
                     item.quality = 0;
---
+++ src/main/java/com/gildedrose/strategy/LegendaryItemUpdateStrategy.java
@@ -0,0 +1,10 @@
+package com.gildedrose.strategy;
+
+import com.gildedrose.Item;
+
+public class LegendaryItemUpdateStrategy implements DailyUpdateStrategy{
+    @Override
+    public void update(Item item) {
+        return;
+    }
+}
