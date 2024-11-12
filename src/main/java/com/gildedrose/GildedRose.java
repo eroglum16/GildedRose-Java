cd87fb85363b7e34400bd5162335e537bbb61310
eroglum16 <m.eroglu5122@gmail.com>
2024-11-12 15:54:15 +0100

    Use strategy context to execute update operation on different types of items

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,39 +2,18 @@ package com.gildedrose;
 
 import com.gildedrose.strategy.*;
 
-import java.util.Arrays;
-import java.util.List;
-
 class GildedRose {
     Item[] items;
 
-    private final List<String> agedProductNames = Arrays.asList("Aged Brie");
-    private final List<String> legendaryItemNames = Arrays.asList("Sulfuras, Hand of Ragnaros");
-    private final List<String> backstagePassNames = Arrays.asList("Backstage passes to a TAFKAL80ETC concert");
-
-    private final DailyUpdateStrategy agedProductDailyUpdateStrategy;
-    private final DailyUpdateStrategy legendaryItemUpdateStrategy;
-    private final DailyUpdateStrategy backstagePassUpdateStrategy;
-    private final DailyUpdateStrategy regularItemUpdateStrategy;
+    private final DailyUpdateStrategyContext dailyUpdateStrategyContext;
     public GildedRose(Item[] items) {
         this.items = items;
-        this.agedProductDailyUpdateStrategy = new AgedProductDailyUpdateStrategy();
-        this.legendaryItemUpdateStrategy = new LegendaryItemUpdateStrategy();
-        this.backstagePassUpdateStrategy = new BackstagePassUpdateStrategy();
-        this.regularItemUpdateStrategy = new RegularItemUpdateStrategy();
+        this.dailyUpdateStrategyContext = new DailyUpdateStrategyContext();
     }
 
     public void updateQuality() {
         for (Item item : items) {
-            if (this.agedProductNames.contains(item.name)){
-                this.agedProductDailyUpdateStrategy.update(item);
-            } else if (this.legendaryItemNames.contains(item.name)){
-                this.legendaryItemUpdateStrategy.update(item);
-            } else if (this.backstagePassNames.contains(item.name)){
-                this.backstagePassUpdateStrategy.update(item);
-            } else {
-                this.regularItemUpdateStrategy.update(item);
-            }
+            dailyUpdateStrategyContext.execute(item);
         }
     }
 }
